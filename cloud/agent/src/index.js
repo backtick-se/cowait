3ea89fc40e86e5696ec8a3c7dce6853344e08b55
Johan Henriksson <johanhenriksson@live.com>
2019-10-13 16:00:08 +0200

    cloud agent: babel & simple object storage

--- cloud/agent/src/index.js
+++ cloud/agent/src/index.js
@@ -1,18 +1,51 @@
-var zmq = require("zeromq")
-var http = require("http")
-var socketio = require("socket.io")
-
-let clients = [ ]
-function handler (req, res) {
-    res.end('hello sir')
-}
+import _ from 'lodash'
+import zmq from 'zeromq'
+import http from 'http'
+import express from 'express'
+import socketio from 'socket.io'
+import { Metastore } from './metastore'
 
 const ZMQ_PORT = 1337
 const WS_PORT = 1338
 
-var app = http.createServer(handler)
-var io = socketio(app)
+let clients = [ ]
 
+var app = express()
+var server = http.Server(app)
+var io = socketio(server)
+
+const metastore = new Metastore([ 'task' ])
+
+function handle_update({ id, type, ...msg }) {
+    switch(type) {
+    case 'init':
+        metastore.set('task', id, msg.task) 
+
+    case 'status':
+        metastore.update('task', id, task => _.set(task, 'status', msg.status))
+
+    case 'return':
+        metastore.update('task', id, task => _.set(task, 'result', msg.result))
+        break
+
+    case 'fail':
+        metastore.update('task', id, task => _.set(task, 'error', msg.error))
+        break
+
+    case 'object':
+        // other metadata
+        metastore.set(msg.kind, msg.data)
+        break
+    }
+}
+
+app.get('/meta/:kind', async (req, res) => {
+    res.json(metastore.getAll(req.params.kind))
+})
+
+app.get('/meta/:kind/:id', async (req, res) => {
+    res.json(metastore.get(req.params.kind, req.params.id))
+})
 
 io.on('connection', client => { 
     clients.push(client)
@@ -33,13 +66,16 @@ console.log('listening for nodes')
 let sock = zmq.socket("pull");
 
 sock.on("message", function(msg) {
-    console.log("work: %s", msg.toString());
-    io.sockets.emit('msg', JSON.parse(msg.toString()))
+    console.log("recv: %s", msg.toString());
+    const event = JSON.parse(msg.toString())
+
+    handle_update(event)
+    io.sockets.emit('msg', event)
 });
 
 sock.bindSync(`tcp://*:${ZMQ_PORT}`);
 
-app.listen(WS_PORT, () => {
+server.listen(WS_PORT, () => {
     console.log('listening for websockets')
 });
 
---
+++ cloud/agent/src/metastore.js
@@ -0,0 +1,93 @@
+import _ from 'lodash'
+
+export class Metastore {
+    constructor(init_tables) {
+        this.tables = { }
+        for (var table of init_tables) {
+            this.initTable(table)
+        }
+    }
+
+    initTable = kind => {
+        const table = new Metatable()
+        this.tables[kind] = table
+        return table
+    }
+
+    getTable = kind => {
+        const table = this.tables[kind]
+        if (!table) {
+            throw new Error(`Unknown meta table ${kind}`)
+        }
+        return table
+    }
+
+    get = (kind, id) => {
+        const table = this.getTable(kind)
+        return table.get(id)
+    }
+
+    getAll = kind => {
+        const table = this.getTable(kind)
+        return table.getAll()
+    }
+
+    set = (kind, id, value) => {
+        let table = this.tables[kind]
+        if (!table) {
+            table = this.initTable(kind)
+        }
+        return table.set(id, value)
+    }
+
+    update = (kind, id, transform) => {
+        const table = this.getTable(kind)
+        return table.update(id, transform)
+    }
+
+    del = (kind, id) => {
+        const table = this.getTable(kind)
+        table.del(id)
+    }
+}
+
+export class Metatable {
+    constructor() {
+        this.order = [ ]
+        this.items = { }
+    }
+
+    get = id => {
+        let item = this.items[id]
+        if (!item) {
+            throw new Error(`No such item ${id}`)
+        }
+        return item
+    }
+
+    getAll = () => {
+        return _.map(this.order, id => this.items[id])
+    }
+
+    set = (id, value) => {
+        if (!_.includes(this.order, id)) {
+            this.order.push(id)
+        }
+        this.items[id] = value
+        console.log(this.items[id])
+    }
+
+    update = (id, transform) => {
+        let item = this.items[id]
+        if (!item) {
+            throw new Error(`No such item ${id}`)
+        }
+        this.items[id] = transform(item)
+        console.log(this.items[id])
+    }
+
+    del = id => {
+        _.remove(this.order, i => i === id)
+        delete this.items[id]
+    }
+}
