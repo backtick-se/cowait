ffca46ab2b10cbbb55dd16eaeebc2f85eb9cd1dc
Johan Henriksson <johanhenriksson@live.com>
2019-11-28 14:35:43 +0100

    do some linting

--- pipeline/engine/cluster.py
+++ pipeline/engine/cluster.py
@@ -3,62 +3,51 @@ import json
 from abc import ABC, abstractmethod
 from typing import Iterable
 from marshmallow import Schema, fields
-from pipeline.tasks import Task, TaskContext, TaskDefinition
+from pipeline.tasks import Task, TaskDefinition
 from .const import ENV_TASK_CLUSTER, ENV_TASK_DEFINITION
 
 
 class ClusterProvider(ABC):
-    def __init__(self, type, args = { }):
-        """
-        """
+    def __init__(self, type, args={}):
         self.type = type
         self.args = args
-
 
     @abstractmethod
     def spawn(self, taskdef: TaskDefinition) -> Task:
         """ Spawn a task in the cluster """
         pass
 
-
     @abstractmethod
     def destroy(self, task_id: str) -> None:
         """ Destroy a task """
         pass
 
-
     @abstractmethod
     def destroy_all(self) -> None:
         pass
-
 
     @abstractmethod
     def destroy_children(self, parent_id: str) -> None:
         pass
 
-    
     @abstractmethod
     def wait(self, task: Task) -> None:
         """ Wait for task to exit """
         pass
-
 
     @abstractmethod
     def logs(self, task: Task) -> Iterable[str]:
         """ Stream logs from task """
         pass
 
-
     def serialize(self) -> dict:
         """ Serialize ClusterProvider into a dict """
         return ClusterProviderSchema().dump(self)
-
 
     @staticmethod
     def deserialize(provider: dict) -> ClusterProvider:
         """ Deserialize ClusterProvider from a dict """
         return ClusterProviderSchema().load(provider)
-
 
     def create_env(self, taskdef: TaskDefinition) -> dict:
         """
@@ -75,7 +64,6 @@ class ClusterProvider(ABC):
             ENV_TASK_CLUSTER:    json.dumps(self.serialize()),
             ENV_TASK_DEFINITION: json.dumps(taskdef.serialize()),
         }
-
 
 
 class ClusterProviderSchema(Schema):
