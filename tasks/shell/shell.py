b607ec590ff60a49133ed9cb8a481aa28a36cb7e
Johan Henriksson <johanhenriksson@live.com>
2019-11-29 20:38:12 +0100

    shell task

--- tasks/shell/shell.py
+++ tasks/shell/shell.py
@@ -4,16 +4,32 @@ from pipeline.tasks import Task
 
 class ShellTask(Task):
     async def run(self, command):
+        # spawn shell command
         process = await asyncio.create_subprocess_shell(
             command,
             stderr=asyncio.subprocess.PIPE,
             stdout=asyncio.subprocess.PIPE,
         )
 
-        def decode(s): return s.decode('utf-8')
-        out, err = map(decode, await process.communicate())
+        # setup stream readers
+        stream_log_to_node(process.stdout, self.node, 'stdout')
+        stream_log_to_node(process.stderr, self.node, 'stderr')
+
+        # wait for process to finish
+        result = await process.wait()
 
         return {
-            'out': out,
-            'err': err,
+            'code': result,
         }
+
+
+def stream_log_to_node(stream, node, name):
+    async def logger():
+        while True:
+            line = await stream.readline()
+            if line == b'':
+                return
+
+            await node.api.log(name, line.decode('utf-8'))
+
+    asyncio.create_task(logger())
