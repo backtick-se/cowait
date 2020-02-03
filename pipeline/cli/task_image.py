288a68a9e1fb33eed27bec9ace490e978e78811c
Johan Henriksson <johanhenriksson@live.com>
2020-02-03 15:01:33 +0100

    improve dockerfile override support

--- pipeline/cli/task_image.py
+++ pipeline/cli/task_image.py
@@ -7,30 +7,21 @@ client = docker.from_env()
 
 
 class TaskImage(object):
-    def __init__(self, context, name, task_path, base_image=None):
-        self.context = context
+    def __init__(self, name, context):
         self.name = name
-        self.path = task_path
-        self.base = base_image if base_image else DEFAULT_BASE_IMAGE
-        self.image = None
+        self.context = context
 
-    def build_custom_base(self, dockerfile):
-        """ Build a custom image and set it as the base image for this task """
-        custom_base, logs = client.images.build(
-            path=os.path.dirname(dockerfile),
-            dockerfile='Dockerfile',
-        )
-        self.base = custom_base.id
-        return logs
-
-    def build(self, requirements=None):
+    def build(self, base: str = None, requirements: str = None):
         """ Build task image """
+
+        if base is None or base == 'default':
+            base = DEFAULT_BASE_IMAGE
 
         # create temporary dockerfile
         df_path = os.path.join(self.context.path, '__dockerfile__')
         with open(df_path, 'w') as df:
             # extend base image
-            print(f'FROM {self.base}', file=df)
+            print(f'FROM {base}', file=df)
 
             # install task-specific requirements
             if requirements:
@@ -42,7 +33,7 @@ class TaskImage(object):
             print('COPY . .', file=df)
 
         # build image
-        self.image, logs = client.images.build(
+        self.image, logs = self.build_image(
             path=self.context.path,
             dockerfile=df_path,
         )
@@ -88,16 +79,20 @@ class TaskImage(object):
         )
 
     @staticmethod
-    def open(context: PipelineContext, task_name: str):
-        task_parts = task_name.split('.')
-        task_path = os.path.join(context.path, *task_parts)
+    def open(task_name: str, context: PipelineContext = None):
+        # automatically create context
+        if context is None:
+            context = PipelineContext.open()
 
-        # ensure directory exists
-        if not os.path.isdir(task_path):
-            raise FileNotFoundError(f'Task directory not found at {task_path}')
+        task_parts = task_name.split('.')
+        task_path = os.path.join(context.root_path, *task_parts)
+        context = context.cwd(task_path)
 
         return TaskImage(
-            context=context,
             name=task_name,
-            task_path=task_path,
+            context=context,
         )
+
+    @staticmethod
+    def build_image(**kwargs):
+        return client.images.build(**kwargs)
