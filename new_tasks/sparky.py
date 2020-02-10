96d83c54f324935f76223f3f212efe759d453933
Johan Henriksson <johanhenriksson@live.com>
2020-02-10 20:35:36 +0100

    update spark task/flow

---
+++ new_tasks/sparky.py
@@ -0,0 +1,74 @@
+import pyspark.sql.functions as F
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType, FloatType
+from pipeline.tasks import sleep
+from pipeline.tasks.spark import SparkFlow
+
+TIMESTAMP = "yyyy-MM-dd'D'HH:mm:ss.SSS"
+
+BitmexTrades = StructType([
+    StructField('timestamp', StringType(), True),
+    StructField('symbol', StringType(), True),
+    StructField('side', StringType(), True),
+    StructField('size', IntegerType(), True),
+    StructField('price', FloatType(), True),
+    StructField('tickDirection', StringType(), True),
+    StructField('trdMatchID', StringType(), True),
+    StructField('grossValue', IntegerType(), True),
+    StructField('homeNotional', FloatType(), True),
+    StructField('foreignNotional', FloatType(), True),
+])
+
+
+class SparkyTask(SparkFlow):
+    async def run(
+        self,
+        spark,
+        filter='2014*',
+        start='2014-01-01',
+        end='2015-01-01',
+        mode='append',
+        **inputs,
+    ):
+        print('bitmex trades csv->parquet')
+
+        await sleep(0.1)
+
+        df = spark.read \
+            .csv(
+                f's3a://stackpoint-spark/data/bitmex/trades/{filter}.csv.gz',
+                header=True,
+                schema=BitmexTrades,
+            )
+
+        df = df.filter(df.timestamp >= start) \
+               .filter(df.timestamp < end)
+
+        df = df.drop('grossValue')
+        df = df.drop('homeNotional')
+        df = df.drop('foreignNotional')
+
+        # cut milli/nanoseconds and convert to timestamp
+        df = df.withColumn("timestamp", F.to_timestamp(F.substring(df.timestamp, 0, 23), TIMESTAMP))
+        df = df.withColumn("time", F.unix_timestamp(df.timestamp))
+        df = df.withColumn("date", F.from_unixtime(df.time, "yyyyMMdd"))
+        df.drop('timestamp')
+
+        df.limit(5).show()
+
+        count = df.count()
+        print(f'{count} trades')
+
+        volume = df.agg(F.sum("size")).collect()[0][0]
+        print(f'total volume: ${volume}')
+
+        print('writing parquet')
+        df.write \
+            .mode(mode) \
+            .partitionBy(['date']) \
+            .parquet('s3a://stackpoint-spark/data/bitmex/parquet/trades')
+
+        print('done writing')
+        return {
+            'trades': count,
+            'volume': volume,
+        }
