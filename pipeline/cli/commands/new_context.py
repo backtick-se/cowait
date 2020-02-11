2e0f95326caae5cbc73141d27bb2363015781f3a
Johan Henriksson <johanhenriksson@live.com>
2020-02-11 23:12:21 +0100

    new context command uses current directory if no name is specified

--- pipeline/cli/commands/new_context.py
+++ pipeline/cli/commands/new_context.py
@@ -1,23 +1,33 @@
 import os
 import os.path
 import yaml
-from ..const import CONTEXT_FILE_NAME
+from ..const import CONTEXT_FILE_NAME, DEFAULT_PROVIDER
 
 
-def new_context(name: str, image: str, provider: str):
-    name = name.lower()
+def new_context(
+    name: str,
+    image: str,
+    provider: str = DEFAULT_PROVIDER,
+    docker_network: str = None,
+    kube_namespace: str = None,
+):
     path = os.getcwd()
 
     # create directory
-    current_dir = os.path.basename(path).lower()
-    if current_dir != name:
-        path = os.path.join(path, name)
+    current_dir = os.path.basename(path)
+    if current_dir != name and name is not None:
+        path = os.path.join(path, name.lower())
         os.mkdir(path)
         print('Created context folder', path)
 
     context = {}
     if image is not None:
+        print('Image:', image)
         context['image'] = image
+
+    context['provider'] = {
+        'default': provider,
+    }
 
     context_file = os.path.join(path, CONTEXT_FILE_NAME)
     if os.path.isfile(context_file):
@@ -33,3 +43,5 @@ def new_context(name: str, image: str, provider: str):
             stream=cf,
             sort_keys=False,
         )
+
+    print('Created new context definition', context_file)
