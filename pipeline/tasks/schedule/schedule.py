4325151bfa8ab464ca66dd1cb5fb8cf87d7941f3
Johan Henriksson <johanhenriksson@live.com>
2020-03-04 16:36:16 +0100

    clean up schedule task

--- pipeline/tasks/schedule/schedule.py
+++ pipeline/tasks/schedule/schedule.py
@@ -1,47 +1,42 @@
 import traceback
 from datetime import datetime
-from pipeline.tasks import Task, sleep
+from pipeline.tasks import Task, TaskError, rpc, sleep
+from pipeline.utils import parse_task_image_name
+from .schedule_definition import ScheduleDefinition
 
 
 class ScheduleTask(Task):
     async def run(self, schedule, target, **inputs):
-        image = 'docker.backtick.se/task'
-        name = target
-        if '/' in target:
-            s = target.rfind('/')
-            image = target[:s]
-            name = target[s+1:]
-            if '/' not in image:
-                # prepend default repo
-                image = f'docker.backtick.se/{image}'
+        image, name = parse_task_image_name(target)
 
-        # validate schedule
-        is_schedule(schedule)
+        # instantiate schedule
+        self.schedule = ScheduleDefinition(schedule)
 
-        print('Scheduled', image, name, 'at', schedule)
+        print(f'Scheduled {name} ({image}) at {schedule}')
+        print(f'The current time is', datetime.now())
 
         last_run = datetime(1970, 1, 1)
         while True:
             await sleep(0.5)
 
             now = datetime.now()
-            if is_schedule(schedule, now):
-                if is_schedule(schedule, last_run) \
+            if self.schedule.is_now():
+                if self.schedule.is_at(last_run) \
                         and last_run.minute == now.minute:
                     continue
 
-                print(f'{now}: Time to run!')
                 last_run = now
-                result = await self.execute(name, image, inputs)
-                print('finished.', result)
+                await self.execute(name, image, inputs)
 
     async def execute(self, name, image, inputs):
+        now = datetime.now()
+        print(f'{now} | Execution triggered')
+
         try:
-            now = datetime.now()
             id = f'{name}-' + \
                  f'{now.year}{now.month:02d}{now.day:02d}' + \
                  f'{now.hour:02d}{now.minute:02d}'
-            return await self.spawn(
+            result = await self.spawn(
                 id=id,
                 name=name,
                 image=image,
@@ -50,60 +45,20 @@ class ScheduleTask(Task):
                 env=self.env,
                 meta=self.meta,
             )
-        except Exception:
+
+            end = datetime.now()
+            duration = end - now
+            print(f'{end} | Finished in {duration}. Result:')
+            print(result)
+
+        except TaskError:
+            end = datetime.now()
+            duration = end - now
+            print(f'{end} | Error after {duration}. Exception:')
             traceback.print_exc()
 
-
-def is_schedule(schedule, time: datetime = None) -> bool:
-    # m h D M DT
-    # * * * * *
-
-    parts = schedule.split(' ')
-    if len(parts) != 5:
-        raise RuntimeError('Invalid schedule syntax')
-
-    minute, hour, date, month, dow = parts
-
-    now = datetime.now() if time is None else time
-    if not is_value(minute, now.minute):
-        return False
-    if not is_value(hour, now.hour):
-        return False
-    if not is_value(date, now.day):
-        return False
-    if not is_value(month, now.month):
-        return False
-    if not is_value(dow, now.weekday()):
-        return False
-
-    return True
-
-
-def is_value(pattern: str, value: int) -> bool:
-    if ',' in pattern:
-        patterns = pattern.split(',')
-        for pattern in patterns:
-            if is_value(pattern, value):
-                return True
-        return False
-
-    if pattern == '*':
-        return True
-
-    if '-' in pattern:
-        dash = pattern.find('-')
-        minimum = int(pattern[:dash])
-        maximum = int(pattern[dash+1:])
-        return value >= minimum and value <= maximum
-
-    if '/' in pattern:
-        slash = pattern.find('/')
-        setting = pattern[:slash]
-        divisor = int(pattern[slash+1:])
-        if setting == '*':
-            return value % divisor == 0
-        else:
-            raise RuntimeError("illegal divisor pattern")
-
-    exact = int(pattern)
-    return value == exact
+    @rpc
+    async def reschedule(self, schedule):
+        # instantiate/validate schedule
+        self.schedule = ScheduleDefinition(schedule)
+        return schedule
