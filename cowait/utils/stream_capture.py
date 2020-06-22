f4e6b463ac3e3bb52fae1ce2dde9001311867575
Johan Henriksson <johanhenriksson@live.com>
2020-06-22 23:22:13 +0200

    StreamCapture should buffer until newline

--- cowait/utils/stream_capture.py
+++ cowait/utils/stream_capture.py
@@ -24,18 +24,26 @@ class StreamCapture(object):
             self.stream.write(data)
 
         if '\n' in data:
-            self.flush()
+            self.flush(auto=True)
 
-    def flush(self):
-        self.capture.flush()
-
+    def flush(self, auto: bool = False):
         if not self.silence:
             self.stream.flush()
 
         if callable(self.callback):
-            self.callback(self.getvalue())
-
-        self.capture = StringIO()
+            value = self.getvalue()
+            if auto:
+                # auto flush happens if \n exists, so we dont need to check for it
+                last_break = value.rfind('\n')
+                self.capture = StringIO(value[last_break+1:])
+                self.callback(value[:last_break+1])
+            else:
+                # non-automated flush should return the full value
+                self.capture = StringIO()
+                self.callback(value)
+        else:
+            # if there is no callback, simply clear the capture buffer
+            self.capture = StringIO()
 
     def getvalue(self):
         data = self.capture.getvalue()
