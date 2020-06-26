6207600573d9540cfa37e88ede53e3db32e42698
Johan Henriksson <johanhenriksson@live.com>
2020-06-27 01:07:06 +0200

    raise an error if task inputs are too large

--- cowait/engine/cluster.py
+++ cowait/engine/cluster.py
@@ -3,7 +3,8 @@ from abc import abstractmethod
 from typing import Iterable
 from cowait.tasks import TaskDefinition, RemoteTask
 from cowait.utils import EventEmitter
-from .const import ENV_TASK_CLUSTER, ENV_TASK_DEFINITION, ENV_GZIP_ENABLED
+from .const import ENV_TASK_CLUSTER, ENV_TASK_DEFINITION, ENV_GZIP_ENABLED, MAX_ENV_LENGTH
+from .errors import ProviderError
 from .utils import env_pack
 
 
@@ -62,12 +63,21 @@ class ClusterProvider(EventEmitter):
         Returns:
             env (dict): Environment variable dict
         """
-        return {
+
+        env = {
             **taskdef.env,
             ENV_GZIP_ENABLED:    '1',
             ENV_TASK_CLUSTER:    env_pack(self.serialize()),
             ENV_TASK_DEFINITION: env_pack(taskdef.serialize()),
         }
+
+        length = 0
+        for key, value in env.items():
+            length += len(key) + len(value)
+        if length > MAX_ENV_LENGTH:
+            raise ProviderError(f'Task input too long. Was {length}, max: {MAX_ENV_LENGTH}')
+
+        return env
 
     def find_agent(self):
         return None
--- cowait/engine/const.py
+++ cowait/engine/const.py
@@ -9,5 +9,17 @@ ENV_TASK_DEFINITION = 'COWAIT_TASK'
 ENV_GZIP_ENABLED = 'COWAIT_GZIP'
 
 
+# ---------------------------------------------------
+# Container label names
+# ---------------------------------------------------
+
 LABEL_TASK_ID = 'cowait/task'
 LABEL_PARENT_ID = 'cowait/parent'
+
+
+# ---------------------------------------------------
+# Settings
+# ---------------------------------------------------
+
+# Maximum length of environment variables, 100kB
+MAX_ENV_LENGTH = 100 * 1024
