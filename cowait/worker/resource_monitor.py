4b61583588b434415db8ee110c31b2b7f7e36e2c
Johan Henriksson <johanhenriksson@live.com>
2020-08-31 14:34:26 +0200

    per-process resource monitoring

--- cowait/worker/resource_monitor.py
+++ cowait/worker/resource_monitor.py
@@ -1,33 +1,31 @@
+import os
 import time
 import psutil
 
 
 class ResourceMonitor():
     def __init__(self):
-        self.last_net = psutil.net_io_counters()
-        self.last_disk = psutil.disk_io_counters()
         self.last_time = time.time()
+        self.process = psutil.Process(os.getpid())
+        self.last_io = self.process.io_counters()
 
     def stats(self):
-        mem = psutil.virtual_memory()
-        cpu = psutil.cpu_percent(percpu=True)
-        net = psutil.net_io_counters()
-        disk = psutil.disk_io_counters()
+        cpu = self.process.cpu_percent()
+        mem = self.process.memory_full_info()
+        io = self.process.io_counters()
         ctime = time.time()
         stats = {
             'dt': round(ctime - self.last_time, 3),
-            'cpu': [ round(c / 100.0, 3) for c in cpu ],
-            'mem': round(1.0 - mem.available / mem.total, 3),
-            'net': [
-                net.bytes_recv - self.last_net.bytes_recv,  # read
-                net.bytes_sent - self.last_net.bytes_sent,  # write
+            'cpu': round(cpu / 100.0, 3),
+            'mem': [
+                mem.uss,  # used
+                psutil.virtual_memory().total,  # total
             ],
-            'disk': [
-                disk.read_bytes - self.last_disk.read_bytes,  # read
-                disk.write_bytes - self.last_disk.write_bytes,  # write
+            'io': [
+                io.read_bytes - self.last_io.read_bytes,  # read
+                io.write_bytes - self.last_io.write_bytes,  # write
             ],
         }
-        self.last_net = net
-        self.last_disk = disk
+        self.last_io = io
         self.last_time = ctime
         return stats
