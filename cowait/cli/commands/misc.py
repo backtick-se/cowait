4f03e26558ff2d75327ba8b1ed7ddd8251e25aeb
emilwareus <emil.wareus47@gmail.com>
2020-09-04 10:01:58 +0200

    kill tasks on string search

--- cowait/cli/commands/misc.py
+++ cowait/cli/commands/misc.py
@@ -29,3 +29,18 @@ def kill(config: CowaitConfig, task_id: str):
 
     except ProviderError as e:
         print('Provider error:', str(e))
+
+
+def kill_name_search(config: CowaitConfig, partial_task_id: str):
+    """Kills all tasks that somewhat matches partial_task_id     
+    """
+    try:
+        cluster = config.get_cluster()
+        tasks = cluster.list_all()
+        for task in tasks:
+            if partial_task_id in str(task):
+                cluster.destroy(str(task))
+                print('Killed ', str(task))
+
+    except ProviderError as e:
+        print('Provider error:', str(e))
