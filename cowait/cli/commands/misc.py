24ea15132a1661ccd6118e1247ca320b966797ac
Johan Henriksson <johanhenriksson@live.com>
2020-11-18 15:43:07 +0100

    change default behaviour of cowait kill

--- cowait/cli/commands/misc.py
+++ cowait/cli/commands/misc.py
@@ -22,25 +22,15 @@ def list_tasks(config: CowaitConfig) -> None:
         print('Provider error:', str(e))
 
 
-def kill(config: CowaitConfig, task_id: str):
-    try:
-        cluster = config.get_cluster()
-        cluster.destroy(task_id)
-
-    except ProviderError as e:
-        print('Provider error:', str(e))
-
-
-def kill_name_search(config: CowaitConfig, partial_task_id: str):
-    """Kills all tasks that somewhat matches partial_task_id     
-    """
+def kill(config: CowaitConfig, partial_task_id: str):
+    """ Kills all tasks that somewhat matches partial_task_id """
     try:
         cluster = config.get_cluster()
         tasks = cluster.list_all()
         for task in tasks:
             if partial_task_id in str(task):
                 cluster.destroy(str(task))
-                print('Killed ', str(task))
+                print('killed ', str(task))
 
     except ProviderError as e:
         print('Provider error:', str(e))
