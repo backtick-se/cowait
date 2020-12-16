0706bdfba1f58d2a5ffb64c99483f09bf226b693
Johan Henriksson <johanhenriksson@live.com>
2020-12-16 15:51:40 +0100

    first attempt at proper dependency versioning

--- setup.py
+++ setup.py
@@ -1,12 +1,9 @@
 import setuptools
 
-VERSION = '0.3.5'
+VERSION = '0.4.0'
 
 with open('README.md', 'r') as fh:
     long_description = fh.read()
-
-with open('requirements.txt', 'r') as fh:
-    requirements = fh.read().splitlines()
 
 setuptools.setup(
     name='cowait',
@@ -30,5 +27,34 @@ setuptools.setup(
     classifiers=[],
 
     python_requires='>=3.7',
-    install_requires=requirements,
+    install_requires=[
+        'docker>=4',
+        'kubernetes>=11,<13',
+        'nest-asyncio>=1.4.1',
+        'aiohttp>=3',
+        'aiohttp-middlewares>=1',
+        'pytest>=6',
+        'alt-pytest-asyncio>=0.5.3',  # would be nice to move to the widely used pytest-asyncio
+        'python-dotenv>=0.15',
+        'fsspec',
+        's3fs',
+        'aiobotocore[boto3]',
+
+        # cli
+        'click>=7',
+        'PyYAML>=5',
+        'sty==1.0.0-beta.12',  # tty coloring, seems to be stuck in beta. remove/replace
+
+        # convenient, but easy to get rid of
+        'marshmallow >= 3',
+
+        # utilities, not actually required
+        'numpy >= 1',  # provides typing for numpy. unlikely to break
+        'pytest-sugar >= 0.9',
+        'pytest-cov >= 2',
+
+        # extract to cowait-dask
+        'dask >= 2',
+        'distributed >= 2',
+    ]
 )
